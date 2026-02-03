8ab97820e28d1fefea0d96c387e17eaa464dc27a
Lyan1204 <ngocluann1204@gmail.com>
2026-02-03 14:36:16 +0700

    ví dụ 1 bài 4

---
+++ Baitap1/src/tiil/edu/HelloWorld.java
@@ -0,0 +1,16 @@
+package tiil.edu;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		//Text blocks voiws """
+		String greeting = """
+				======================
+				HELLO, MODERN JAVA!
+				======================
+				by Mai Cường Thọ NTU
+				""";
+		System.out.println("greeting");
+	}
+
+}
