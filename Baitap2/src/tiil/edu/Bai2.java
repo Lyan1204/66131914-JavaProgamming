5c985db68ee008a9d590be0354e70ec0c22b5a9c
Lyan1204 <ngocluann1204@gmail.com>
2026-02-03 14:54:49 +0700

    Create Bai2.java

---
+++ Baitap2/src/tiil/edu/Bai2.java
@@ -0,0 +1,16 @@
+package tiil.edu;
+
+public class Bai2 {
+
+	public static void main(String[] args) {
+		//Chuyển từ Calsius sang Fahrenheit
+		//Công thức: F - (C x 9/5) + 32
+		
+		double celsius = 50;
+		double fahrenheit = (celsius * 9/5) + 32;
+		
+		System.out.println(celsius + "C = " + fahrenheit + "F");
+
+	}
+
+}
