569266efabb75f33acbee77a1284f29ba4b0c8b7
Lyan1204 <ngocluann1204@gmail.com>
2026-02-03 14:26:58 +0700

    Ví dụ bài 2
    nộp bài

---
+++ Baitap1/src/tiil/edu/Bai2.java
@@ -0,0 +1,13 @@
+package tiil.edu;
+
+public class Bai2 {
+
+	public static void main(String[] args) {
+		System.out.println("   *   ");
+		System.out.println("  ***  ");
+		System.out.println(" ***** ");
+		System.out.println("*******");
+
+	}
+
+}
