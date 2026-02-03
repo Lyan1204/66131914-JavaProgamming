90b9def2682704e28d1bb9308e7f02c31d9906cb
Lyan1204 <ngocluann1204@gmail.com>
2026-02-03 14:40:21 +0700

    Create HelloWorld2.java

---
+++ Baitap1/src/tiil/edu/HelloWorld2.java
@@ -0,0 +1,16 @@
+package tiil.edu;
+
+public class HelloWorld2 {
+
+	public static void main(String[] args) {
+		// var - tự động suy luận kiểu
+		var message = "Hello, World with Java!";
+		var number = 42;
+		var list = java.util.List.of("Java", "Modern", "Features");
+		
+		System.out.println("message");
+		System.out.println("Number: " + number);
+		System.out.println("List: " + list);
+	}
+
+}
