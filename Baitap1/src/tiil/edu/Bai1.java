0ca64f833432130695f946295420c871ecf037ba
Lyan1204 <ngocluann1204@gmail.com>
2026-02-03 14:20:00 +0700

    Ví dụ bài 1
    Nộp bài

---
+++ Baitap1/src/tiil/edu/Bai1.java
@@ -0,0 +1,14 @@
+package tiil.edu;
+
+/**
+ * 
+ */
+public class Bai1 {
+	public static void main(String[] args) {
+		System.out.println("Xin chào thầy và các bạn");
+		System.out.println("Mình tên là Nguyễn Ngọc Luân, sinh viên năm 2 ngành công nghệ thông tin, Đại học Nha Trang");
+		System.out.println("Mình 20 tuổi, sống và học tập ở Nha Trang");
+
+	}
+
+}
