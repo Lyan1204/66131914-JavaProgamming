cce9c6cb15833b81d388b12dd2377245fb27d0be
Lyan1204 <ngocluann1204@gmail.com>
2026-02-10 12:15:50 +0700

    Up bai

---
+++ Bai1-Thaydoithongdiep/src/Bai1.java
@@ -0,0 +1,11 @@
+
+public class Bai1 {
+
+	public static void main(String[] args) {
+		System.out.println("Xin chào thầy và các bạn!");
+		System.out.println("Chào mừng đến với môn học lập trình Java!");
+		System.out.println("Mình tên là Nguyễn Ngọc Luân, sinh viên năm 2, Trường Đại học Nha Trang");
+
+	}
+
+}
