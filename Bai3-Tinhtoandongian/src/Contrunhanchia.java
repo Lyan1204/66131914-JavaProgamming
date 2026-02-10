afe86eee9bf3d1d195b49481f052e6fb00d87d13
Lyan1204 <ngocluann1204@gmail.com>
2026-02-10 12:53:27 +0700

    up code

---
+++ Bai3-Tinhtoandongian/src/Contrunhanchia.java
@@ -0,0 +1,15 @@
+
+public class Contrunhanchia {
+
+	public static void main(String[] args) {
+		double so1= 36;
+		double so2= 18;
+		System.out.println("Phép nhân: " + so1 + " x " + so2 + " = " + (so1 * so2));
+		System.out.println("Phép chia: " + so1 + " : " + so2 + " = " + (so1 / so2));
+		System.out.println("Phép cộng:  " + so1 + " + " + so2 + " = " + (so1 + so2));
+		System.out.println("Phép trừ: " + so1 + " - " + so2 + " = " + (so1 - so2));
+		
+
+	}
+
+}
