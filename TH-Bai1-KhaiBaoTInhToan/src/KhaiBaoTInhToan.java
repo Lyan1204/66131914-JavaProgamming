e396eddbe0509e16a7358dc395970a84af448fa2
Lyan1204 <ngocluann1204@gmail.com>
2026-02-10 13:44:50 +0700

    up code

---
+++ TH-Bai1-KhaiBaoTInhToan/src/KhaiBaoTInhToan.java
@@ -0,0 +1,22 @@
+
+public class KhaiBaoTInhToan {
+
+	public static void main(String[] args) {
+		//Khai báo biến
+		int a = 12;
+		int b = 4;
+		double c = 2006;
+		
+		//Tính toán
+		int Tong = a + b;
+		double Thuong = c / a;
+		int PhanDu = a;
+		
+		//In kết quả
+		System.out.println("Tổng: " + Tong);
+		System.out.println("Thương: " + Thuong);
+		System.out.println("Phần dư: " + PhanDu);
+
+	}
+
+}
