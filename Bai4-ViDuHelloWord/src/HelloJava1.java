c20b70bbd2f1f957b75aa3a182c7cfc78a4ab8cf
Lyan1204 <ngocluann1204@gmail.com>
2026-02-10 13:28:05 +0700

    up code

---
+++ Bai4-ViDuHelloWord/src/HelloJava1.java
@@ -0,0 +1,16 @@
+
+public class HelloJava1 {
+
+	public static void main(String[] args) {
+		String greenting = """
+				==================
+				HELLO, MODERN JAVA!
+				==================
+				by Nguyễn Ngọc Luân
+				==================
+				""";
+		System.out.println(greenting);
+
+	}
+
+}
---
+++ Bai4-ViDuHelloWord/src/HelloJava2.java
@@ -0,0 +1,14 @@
+
+public class HelloJava2 {
+
+	public static void main(String[] args) {
+		var message = "Hello, world with var!";
+		var number = 12;
+		var list = java.util.List.of("Java", "Modern", "Features");
+		
+		System.out.println("message");
+		System.out.println("Number: " + number);
+		System.out.println("List: " + list);
+	}
+
+}
