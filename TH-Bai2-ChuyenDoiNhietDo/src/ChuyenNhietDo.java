193cf1aa598681f90b1ad26147791fd88c31e38e
Lyan1204 <ngocluann1204@gmail.com>
2026-02-10 13:56:10 +0700

    up code thực hành

---
+++ TH-Bai2-ChuyenDoiNhietDo/src/ChuyenNhietDo.java
@@ -0,0 +1,14 @@
+
+public class ChuyenNhietDo {
+
+	public static void main(String[] args) {
+		//Chuyển từ Celsius sang Fahrenheit 
+		//Công thức: F = (C x 9/5) + 32
+		
+		double celsius = 36.5;
+		double fahrenheit = (celsius * 9/5) + 32;
+		
+		System.out.println(celsius + " Độ C = " + fahrenheit + " Độ F");
+	}
+
+}
