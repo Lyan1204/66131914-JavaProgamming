2f8f948defcca38b2cb7bf4cda6e0fdd1e447620
Lyan1204 <ngocluann1204@gmail.com>
2026-02-10 17:23:48 +0700

    up file

---
+++ TH-Bai4-TienDien/src/TinhTienDien.java
@@ -0,0 +1,11 @@
+
+public class TinhTienDien {
+
+	public static void main(String[] args) {
+		/*Tính tiền điện theo bậc thang:
+		 - 0-50 kWh: 1,678 đ/kWh
+		 - 51-100 kWh: 1,734
+		 */
+	}
+
+}
