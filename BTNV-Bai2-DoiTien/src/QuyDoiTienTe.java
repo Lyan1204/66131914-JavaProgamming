57a68d50c5f8aa1d701d56b10d9b071cb3532534
Lyan1204 <ngocluann1204@gmail.com>
2026-02-11 13:52:00 +0700

    Update QuyDoiTienTe.java

--- BTNV-Bai2-DoiTien/src/QuyDoiTienTe.java
+++ BTNV-Bai2-DoiTien/src/QuyDoiTienTe.java
@@ -1,8 +1,35 @@
-
+import java.util.Scanner;
 public class QuyDoiTienTe {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		//Quy đổi tiền tệ
+		//USD sang VND (1 USD = 23,500 VND)
+		//EUR sang VND (1 EUR = 27,000 VND)
+		
+		//Tạo đối tượng Scanner
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		double USD = 23500;
+		double EUR = 27000;
+		
+		System.out.print("Nhập số tiền: ");
+		double SoTien = scanner.nextDouble();
+		
+		System.out.print("Nhập loại tiền (USD/EUR): ");
+		String LoaiTien = scanner.next().toUpperCase();
+		
+		double TienViet = 0;
+		
+		if(LoaiTien.equals("USD")) {
+			TienViet = SoTien * USD;
+		} else if(LoaiTien.equals("EUR")) {
+			TienViet = SoTien * EUR;
+		}
+		
+		System.out.println("Doi tien: " + String.format("%,.0f", TienViet) + " VND");
+		
+		scanner.close();
 
 	}
 
