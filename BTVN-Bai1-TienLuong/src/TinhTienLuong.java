11f4ac5b8e9cfd1cb88b52434ef56c76fcbf67e8
Lyan1204 <ngocluann1204@gmail.com>
2026-02-11 11:11:37 +0700

    up code

---
+++ BTVN-Bai1-TienLuong/src/TinhTienLuong.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+public class TinhTienLuong {
+
+	public static void main(String[] args) {
+		//Tính lương nhân viên dựa vào số giờ làm việc
+		//Nếu làm trên 40 giờ, những giờ vượt tính 1.5 lần
+		
+		//Tạo đối tượng Scanner
+		Scanner scanner = new Scanner(System.in);
+		
+		double SoGioLam;
+		double LuongTheoGio;
+		double TongLuong;
+
+		System.out.println("Nhập số giờ làm: ");
+		SoGioLam = scanner.nextDouble();
+		System.out.println("Nhập số lương theo giờ: ");
+		LuongTheoGio = scanner.nextDouble();
+		
+		if(SoGioLam <= 40) {
+			TongLuong = SoGioLam * LuongTheoGio;
+		} else {
+			TongLuong = 40 * LuongTheoGio + (SoGioLam - 40) * LuongTheoGio * 1.5;
+		}
+		
+		System.out.println("Số giờ làm: " + SoGioLam + " giờ");
+		System.out.println("Lương theo giờ: " + LuongTheoGio + " đồng");
+		System.out.println("Tổng lương: " + TongLuong + " đồng");
+		System.out.println("Tổng lương (VND): " + String.format("%,.0f", TongLuong));
+	}
+
+}
