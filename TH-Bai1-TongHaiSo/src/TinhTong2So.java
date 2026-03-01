0660cb6f707223f0ac11ddd0b0016d18f40b86e3
Lyan1204 <ngocluann1204@gmail.com>
2026-03-01 11:52:34 +0700

    Chủ đề 3, bài 1, tính tổng 2 số

---
+++ TH-Bai1-TongHaiSo/src/TinhTong2So.java
@@ -0,0 +1,21 @@
+
+import java.util.Scanner;
+
+public class TinhTong2So {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.print("Nhập số thứ nhất: ");
+		double num1 = scanner.nextDouble();
+		
+		System.out.print("Nhập số thứ 2: ");
+		double num2 = scanner.nextDouble();
+
+		double sum = num1 + num2;
+		
+		System.out.printf("Tổng của %.2f và %.2f là: %.2f%n", num1, num2, sum);
+		
+		scanner.close();
+	}
+}
