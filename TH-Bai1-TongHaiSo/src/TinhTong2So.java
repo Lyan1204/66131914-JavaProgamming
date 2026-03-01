1b532a862272ea5c3e3546214a80689b13b93958
Lyan1204 <ngocluann1204@gmail.com>
2026-03-01 11:55:29 +0700

    Update TinhTong2So.java

--- TH-Bai1-TongHaiSo/src/TinhTong2So.java
+++ TH-Bai1-TongHaiSo/src/TinhTong2So.java
@@ -15,6 +15,7 @@ public class TinhTong2So {
 		double sum = num1 + num2;
 		
 		System.out.printf("Tổng của %.2f và %.2f là: %.2f%n", num1, num2, sum);
+		System.out.printf("Tổng của %.2f và %.2f là: %.2f", num1, num2, sum);
 		
 		scanner.close();
 	}
