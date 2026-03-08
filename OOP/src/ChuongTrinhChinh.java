361740ab13257de98399bf5336fb70e858cb7d4c
Lyan1204 <ngocluann1204@gmail.com>
2026-03-09 01:22:24 +0700

    OOP sinh vien

---
+++ OOP/src/ChuongTrinhChinh.java
@@ -0,0 +1,13 @@
+
+public class ChuongTrinhChinh {
+
+	public static void main(String[] args) {
+		SinhVien sv1 = new SinhVien();
+		
+		sv1.maSV = "66131914";
+		sv1.ten = "Nguyễn Ngọc Luân";
+		sv1.tuoi = 19;
+		sv1.HocBai("Lập trình Java");
+	}
+
+}
---
+++ OOP/src/SinhVien.java
@@ -0,0 +1,11 @@
+
+public class SinhVien {
+
+	String ten;
+	int tuoi;
+	String maSV;
+	
+	void HocBai(String monHoc) {
+		System.out.println(ten + " đang học môn " + monHoc);
+	}
+}
