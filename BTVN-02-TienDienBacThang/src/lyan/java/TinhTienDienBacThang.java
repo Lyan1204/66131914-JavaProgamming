e59deb00abf7cbc73dadbf1929c65973185305f7
Lyan1204 <ngocluann1204@gmail.com>
2026-03-15 15:43:39 +0700

    Update TinhTienDienBacThang.java

--- BTVN-02-TienDienBacThang/src/lyan/java/TinhTienDienBacThang.java
+++ BTVN-02-TienDienBacThang/src/lyan/java/TinhTienDienBacThang.java
@@ -1,9 +1,21 @@
 package lyan.java;
 
+import java.util.Scanner;
+
 public class TinhTienDienBacThang {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		//Viết chương trình tính tiền điện với bậc thang
+		/*	0-50 kWh = 1.678/kWh
+			51-100 kWh = 1.678/kWh
+			101-200 kWh = 1.678/kWh
+			201-300 kWh = 1.678/kWh
+			301 kWh = 1.678/kWh
+		*/
+		
+	Scanner scanner = new Scanner(System.in);
+	
+	
 
 	}
 
