69e1a9b567020f43cd936144f3fba9afc3fe8b57
Lyan1204 <ngocluann1204@gmail.com>
2026-04-02 00:37:11 +0700

    Update HocSinhMain.java

--- ViDu5-OOP-ArrayList&Class/src/lyan/java/HocSinhMain.java
+++ ViDu5-OOP-ArrayList&Class/src/lyan/java/HocSinhMain.java
@@ -1,5 +1,7 @@
 package lyan.java;
 
+import java.util.Scanner;
+
 public class HocSinhMain {
 
 	public static void main(String[] args) {
@@ -24,6 +26,13 @@ public class HocSinhMain {
 		System.out.print("\nTuổi của học sinh 2 là: " + hs2.getTuoiHS());
 		System.out.print("\nLớp của học sinh 2 là: " + hs2.getLopHS());
 		
+		
+		//=============NHẬP DƯ LIỆU TỪ BÀN PHÍM===========
+		Scanner banPhim = new Scanner(System.in);
+		// Nhập dữ liệu cho  học sinh thứ 3
+		// Hỏi user nhập vào tên, tuổi, lớp
+		System.out.print("\nMời nhập thông tin của học sinh thứ 3");
+		banPhim.nextLine();
 		
 		
 	}
