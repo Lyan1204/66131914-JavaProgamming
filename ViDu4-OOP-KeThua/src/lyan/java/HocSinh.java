ef71261f70e5adcf245ded292cc827f5495bda4c
Lyan1204 <ngocluann1204@gmail.com>
2026-04-01 16:04:53 +0700

    Update HocSinh.java

--- ViDu4-OOP-KeThua/src/lyan/java/HocSinh.java
+++ ViDu4-OOP-KeThua/src/lyan/java/HocSinh.java
@@ -1,5 +1,47 @@
 package lyan.java;
 
 public class HocSinh {
+	private String tenHS;
+	private int tuoiHS;
+	private String lopHS;
+	
+	public HocSinh(String tenHS, int tuoiHS, String lopHS) {
+		this.tenHS = tenHS;
+		this.tuoiHS = tuoiHS;
+		this.lopHS = lopHS;
+	}
 
+	// Tạo getter & setter
+	public String getTenHS() {
+		return tenHS;
+	}
+
+	public void setTenHS(String tenHS) {
+		this.tenHS = tenHS;
+	}
+
+	public int getTuoiHS() {
+		return tuoiHS;
+	}
+
+	public void setTuoiHS(int tuoiHS) {
+		this.tuoiHS = tuoiHS;
+	}
+
+	public String getLopHS() {
+		return lopHS;
+	}
+
+	public void setLopHS(String lopHS) {
+		this.lopHS = lopHS;
+	}
+
+	// Tạo phương thức toString
+	@Override
+	public String toString() {
+		return "HocSinh [tenHS=" + tenHS + ", tuoiHS=" + tuoiHS + ", lopHS=" + lopHS + "]";
+	}
+	
+	
+	
 }
