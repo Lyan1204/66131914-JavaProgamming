605d7a3c45b3d8e4a074739c7c7c20e79ca9da3e
Lyan1204 <ngocluann1204@gmail.com>
2026-04-01 16:45:38 +0700

    Update HocSinhMain.java

--- ViDu5-OOP-ArrayList&Class/src/lyan/java/HocSinhMain.java
+++ ViDu5-OOP-ArrayList&Class/src/lyan/java/HocSinhMain.java
@@ -3,8 +3,29 @@ package lyan.java;
 public class HocSinhMain {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		// Tạo mới 2 học sinh, hard-code dữ liệu
+		
+		HocSinh hs1 = new HocSinh();
+		
+		String tenHS1 = "Nguyễn Ngọc Luân";
+		hs1.setTenHS(tenHS1);
+		short tuoiHS1 = 18;
+		hs1.setTuoiHS(tuoiHS1);
+		
+		String lopHS1 = "12C3";
+		hs1.setLopHS(lopHS1);
+		
+		HocSinh hs2 = new HocSinh("Nguyễn Minh Ánh", (short)17, "11B5");
+		
+		// In ra màn hình
+		System.out.print(hs1.toString());
+		
+		System.out.print("\nTên của học sinh 2 là: " + hs2.getTenHS());
+		System.out.print("\nTuổi của học sinh 2 là: " + hs2.getTuoiHS());
+		System.out.print("\nLớp của học sinh 2 là: " + hs2.getLopHS());
+		
+		
+		
 	}
 
 }
