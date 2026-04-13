d9c41f04caaf091160aa462c51fde19b917fb365
Lyan1204 <ngocluann1204@gmail.com>
2026-04-13 16:16:02 +0700

    done laptop and maytinhbang

--- TH04-Bai1-HeThongThietBiDienTu/src/lyan/java/Laptop.java
+++ TH04-Bai1-HeThongThietBiDienTu/src/lyan/java/Laptop.java
@@ -1,5 +1,41 @@
 package lyan.java;
 
-public class Laptop {
+public class Laptop extends ThietBiDienTu implements CoTheKetNoiVoiInternet {
+	
+	private String cpu;
+	private int ram;
+	
+	
+	public Laptop(String ten, double gia, String hangSanXuat, String cpu, int ram) {
+		super(ten, gia, hangSanXuat);
+		this.cpu = cpu;
+		this.ram = ram;
+	}
 
+	@Override
+	public void bat() {
+		System.out.println("[" + ten + "] Laptop đang khởi động CPU" + cpu);
+	}
+	
+	@Override 
+	public void tat() {
+		System.out.println("[" + ten + "] Laptop đang Shutdown");
+	}
+	
+	@Override
+	public void ketNoiWifi(String tenMang) {
+		System.out.println("[" + ten + "] Laptop đã kết nối với wifi");
+	}
+	
+	@Override void ngatKetNoi() {
+		System.out.println("[" + ten + "] Laptop đã ngắt kết nối với wifi");
+	}
+	
+	@Override
+	public void hienThiThongTin() {
+		super.hienThiThongTin();
+		System.out.println("CPU: " + cpu);
+		System.out.println("Ram: " + ram);
+	}
+	
 }
--- TH04-Bai1-HeThongThietBiDienTu/src/lyan/java/MayTinhBang.java
+++ TH04-Bai1-HeThongThietBiDienTu/src/lyan/java/MayTinhBang.java
@@ -1,5 +1,47 @@
 package lyan.java;
 
-public class MayTinhBang {
+public class MayTinhBang  extends ThietBiDienTu implements CoTheKetNoiVoiInternet, CoTheCamUng{
+	
+	private double kichThuocManHinh;
 
+	public MayTinhBang(String ten, double gia, String hangSanXuat, double kichThuocManHinh) {
+		super(ten, gia, hangSanXuat);
+		this.kichThuocManHinh = kichThuocManHinh;
+	}
+	
+	@Override
+	public void bat() {
+		System.out.println("[" + ten + "] Máy tính bảng đang khởi động");	
+	}
+	
+	@Override 
+	public void tat() {
+		System.out.println("[" + ten + "] Máy tính bảng đang tắt");
+	}
+	
+	@Override
+	public void ketNoiWifi(String tenMang) {
+		System.out.println("[" + ten + "] Máy tính bảng đã kết nối wifi" + tenMang);
+	}
+	
+	@Override
+	public void ngatKetNoi() {
+		System.out.println("[" + ten + "] Máy tính bảng đã ngắt kết nối wifi");
+	}
+	
+	@OVerride
+	public void cham(int x, int y) {
+		System.out.println("[" + ten + "] Chạm tại (" + x + "," + y + ")");
+	}
+	
+	@Override
+	public void vuot(String huong) {
+		System.out.println("[" + ten + "] Vuốt" + huong);
+	}
+	
+	@Override
+	public void hienThiThongTin() {
+		super.hienThiThongTin();
+		System.out.println("Màn hình: " + kichThuocManHinh + "inch");
+	}
 }
