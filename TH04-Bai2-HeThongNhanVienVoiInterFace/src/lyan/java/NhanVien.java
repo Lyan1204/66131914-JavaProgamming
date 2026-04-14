b666fa89b1287128594827de1863f415d9cc2296
Lyan1204 <ngocluann1204@gmail.com>
2026-04-14 15:37:05 +0700

    Tạo file

---
+++ TH04-Bai2-HeThongNhanVienVoiInterFace/src/lyan/java/NhanVien.java
@@ -0,0 +1,5 @@
+package lyan.java;
+
+public class NhanVien {
+
+}
---
+++ TH04-Bai2-HeThongNhanVienVoiInterFace/src/lyan/java/NhanVienMain.java
@@ -0,0 +1,10 @@
+package lyan.java;
+
+public class NhanVienMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
