e6ae93ebace0873f3717f265eeb6ad01ef6d19ad
Lyan1204 <ngocluann1204@gmail.com>
2026-04-14 15:16:52 +0700

    tạo hàm main

--- TH04-Bai1-HeThongThietBiDienTu/src/lyan/java/MainDienTu.java
+++ TH04-Bai1-HeThongThietBiDienTu/src/lyan/java/MainDienTu.java
@@ -3,8 +3,43 @@ package lyan.java;
 public class MainDienTu {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		//Tạo các thiết bị
+		DienThoaiThongMinh dt = new DienThoaiThongMinh("Iphone 15", 25_000_000, "Apple", "iOs 17", 3877);
+		
+		MayTinhBang mtb = new MayTinhBang("iPad Air", 18_000_000, "Apple", 10.9);
+		
+		Laptop lp = new Laptop("ThinkBook 16", 22_000_000, "Lenovo", "AMD Ryzen 7 H255", 16);
+		
+		System.out.println("\nĐIỆN THOẠI THÔNGH MINH");
+		//Điện thoại
+		dt.hienThiThongTin();
+		dt.bat();
+		dt.tat();
+		dt.ketNoiWifi("HomeWifi_5G");
+		dt.cham(200, 500);
+		dt.vuot("trái");
+		dt.ngatKetNoi();
+		dt.tat();
+		
+		
+		//Máy tính bảng
+		System.out.println("/nMÁY TÍNH BẢNG");
+		mtb.hienThiThongTin();
+		mtb.bat();
+		mtb.ketNoiWifi("CafeWifi");
+		mtb.vuot("lên");
+		mtb.tat();
+		
+		
+		
+		//Laptop
+		System.out.println("LAPTOP");
+		lp.hienThiThongTin();
+		lp.bat();
+		lp.ketNoiWifi("OfficeWifi");
+		lp.ngatKetNoi();
+		lp.tat();		
 	}
 
 }
