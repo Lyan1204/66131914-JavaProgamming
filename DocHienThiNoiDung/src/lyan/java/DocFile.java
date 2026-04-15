d29d8d3e00362c2ec0cc33b1b4b5459c5e830553
Nguyễn Ngọc Luân <ngocluann1204@gmail.com>
2026-04-15 15:39:29 +0700

    tao doc chuoi va done doc van ban

---
+++ DocHienThiNoiDung/src/lyan/java/DocFile.java
@@ -0,0 +1,10 @@
+package lyan.java;
+
+public class DocFile {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ DocVanBan/src/lyan/java/KtoD.java
@@ -0,0 +1,40 @@
+package lyan.java;
+
+import java.io.*;
+
+public class KtoD {
+
+	public static void main(String[] args) {
+		
+		String str;
+		
+		FileWriter fw;
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		try {
+			fw = new FileWriter("testUnicode.txt");
+		}
+		
+		catch(IOException exc) {
+			System.out.println("Khong the mo file."); return;
+		}
+		
+		System.out.println("Nhap ('stop' de ket thuc chuong trinh).");
+		
+		do {
+			System.out.print(": ");
+			
+			str = br.readLine();
+			
+			if(str.compareTo("stop") == 0) break;
+			
+			str = str + "\r\n";
+			
+			fw.write(str);
+			
+		} while(str.compareTo("stop") != 0);
+		fw.close();
+		
+	}
+
+}
