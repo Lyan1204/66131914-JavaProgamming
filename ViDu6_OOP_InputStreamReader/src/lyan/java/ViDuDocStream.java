8f8b344bfb1c5731d1401fceeefe8e8acf8eeb9c
Nguyễn Ngọc Luân <ngocluann1204@gmail.com>
2026-04-15 10:03:19 +0700

    Update ViDuDocStream.java

--- ViDu6_OOP_InputStreamReader/src/lyan/java/ViDuDocStream.java
+++ ViDu6_OOP_InputStreamReader/src/lyan/java/ViDuDocStream.java
@@ -2,9 +2,22 @@ package lyan.java;
 
 public class ViDuDocStream {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws IOExeception {
+		
+		//Khai báo biến đối tượng InputStreamReader
+		
+		InputStreamReader ipReader = new InputStreamReader(System.in);
+		
+		//Khai báo biến đối tượng Buffer
+		
+		BufferedReader bufReader = new BufferedReader(ỉReader);
+		
+		//Đọc dữ liệu từ bàn phím, cất vào biến chuỗi
+		String lineDocDuoc = bufReader.readLine();
+		
+		
+		
+		
 	}
 
 }
