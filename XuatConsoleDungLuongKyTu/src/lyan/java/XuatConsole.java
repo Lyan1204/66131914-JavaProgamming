fa361a77fc68ffdce4d20fec6414350279942cd0
Nguyễn Ngọc Luân <ngocluann1204@gmail.com>
2026-04-15 15:23:18 +0700

    XuatConsole

---
+++ XuatConsoleDungLuongKyTu/src/lyan/java/XuatConsole.java
@@ -0,0 +1,22 @@
+package lyan.java;
+
+import java.io.*;
+
+public class XuatConsole {
+
+	public static void main(String[] args) {
+		
+		PrintWriter pw = new PrintWriter(System.out, true);
+		
+		int i = 10;
+		double d = 123.67;
+		double r = i + d;
+		
+		pw.println("Using a PrintWriter.");
+		pw.println(i);
+		pw.println(d);
+		pw.println(i + " + " + d + " = " + r);
+		
+	}
+
+}
