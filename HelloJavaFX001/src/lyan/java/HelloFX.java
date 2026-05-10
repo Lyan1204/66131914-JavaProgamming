e272837c2315150736ff487f1f62ba57b1574d73
Nguyễn Ngọc Luân <ngocluann1204@gmail.com>
2026-05-10 11:08:36 +0700

    Update HelloFX.java

--- HelloJavaFX001/src/lyan/java/HelloFX.java
+++ HelloJavaFX001/src/lyan/java/HelloFX.java
@@ -1,10 +1,28 @@
 package lyan.java;
 
-public class HelloFX {
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.control.Button;
 
+
+public class HelloFX extends Application{
+
+	@Override
+	public void start(Stage primaryStage) throws Exception{
+		// Tạo một nút bấm
+		Button btn = new Button();
+		
+		
+		//
+		btn.setText("")
+		
+	}
+	
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		
+		
 	}
 
 }
