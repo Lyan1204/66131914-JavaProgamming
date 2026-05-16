2bd2c6bd897819b832b941eaf88273de9908845e
Nguyễn Ngọc Luân <ngocluann1204@gmail.com>
2026-05-16 11:20:06 +0700

    Update HelloFX.java

--- HelloJavaFX001/src/lyan/java/HelloFX.java
+++ HelloJavaFX001/src/lyan/java/HelloFX.java
@@ -3,6 +3,7 @@ package lyan.java;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
 
 
 public class HelloFX extends Application{
@@ -14,13 +15,26 @@ public class HelloFX extends Application{
 		
 		
 		//
-		btn.setText("")
+		btn.setText("Xin chao JavaFX")
+		// Tao cua so 
+		StackPane root = new StacPane();
+		
+		root.getChildren().add(btn);
+		
+		// Màn hình / cảnh hiển thị
+		Scene scn = new Scene(root, 400, 200);
+		
+		primaryStage.setTitle("Hello World!");
+		primaryStage.setScene(scn);
+		primaryStage.show();
+		
+		 
 		
 	}
 	
 	
 	public static void main(String[] args) {
-
+		Launch(args);
 		
 		
 	}
